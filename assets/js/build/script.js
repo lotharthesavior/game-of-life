5d419db54f64b69e5b315a62566aa8427a9fdaa8
Savio Resende <savioresende@Savios-MBP.hitronhub.home>
2017-01-15 13:54:25 -0800

    Tested rules and implemented the solution. Solved some bugs.

--- assets/js/build/script.js
+++ assets/js/build/script.js
@@ -6,152 +6,445 @@
  * @param integer width
  * @param integer height
  */
-var Board = function(element, width, height){
+var Board = function (element, width, height) {
 
-	// clean board
-	$('#board').html('');
+    // clean board
+    $('#board').html('');
 
-	var BoardMethods = {};
+    var BoardMethods = {};
 
-	BoardMethods.grid = [];
+    BoardMethods.grid = [];
+    BoardMethods.grid2 = [];
 
-	/**
-	 * 
-	 */
-	function createAbstractGrid(){
-		for(var i = 0;i < width; i++){
-			BoardMethods.grid.push([]);
+    BoardMethods.heartBeat;
 
-			for(var i2 = 0;i2 < height; i2++){
-				BoardMethods.grid[i].push(false);
-			}
-		}
-	}
+    /**
+     * Create the abstract grids
+     */
+    function createAbstractGrid(){
+        for(var i = 0;i < width; i++){
+            BoardMethods.grid.push([]);
+            BoardMethods.grid2.push([]);
 
-	/**
-	 *
-	 */
-	function createPhysicalGrid(){
-		// loop into the lines
-		BoardMethods.grid.forEach(function(element, index){
-			var newElement = document.createElement("div");
-			newElement.classList.add('line');
+            for(var i2 = 0;i2 < height; i2++){
+                BoardMethods.grid[i].push(false);
+                BoardMethods.grid2[i].push(false);
+            }
+        }
+    }
 
-			BoardMethods.grid[index].forEach(function(element2, index2){
-				var newElement2 = document.createElement("div");
-				newElement2.classList.add('column');
-				$(newElement).append(newElement2);
-			});
+    /**
+     * Create the DOM
+     */
+    function createPhysicalGrid(){
+        // loop into the lines
+        BoardMethods.grid.forEach(function(element, index){
+            var newElement = document.createElement("div");
+            newElement.classList.add('line');
 
-			$('#board').append(newElement);
-		});
-	}
+            BoardMethods.grid[index].forEach(function(element2, index2){
+                var newElement2 = document.createElement("div");
+                newElement2.classList.add('column');
+                $(newElement).append(newElement2);
+            });
 
-	/**
-	 * build Abstract Grid
-	 * 
-	 @ @todo the original state of the cell will be random
-	 * @param integer width
-	 * @param integer height
-	 */
-	(function(){
+            $('#board').append(newElement);
+        });
+    }
 
-		createAbstractGrid();
+    /**
+     * build Abstract Grid
+     * 
+     @ @todo the original state of the cell will be random
+     * @param integer width
+     * @param integer height
+     */
+    (function(){
 
-		createPhysicalGrid();
+        createAbstractGrid();
 
-	})();
+        createPhysicalGrid();
 
-	/**
-	 * build Grid
-	 *
-	 * @param $('#element') element
-	 * @param integer width
-	 * @param integer height
-	 */
-	(function(){
-		// var btn = document.createElement("BUTTON");
-	})();
+    })();
 
-	/**
-	 * Return an Array of neighbours
-	 * 
-	 * @param integer line
-	 * @param integer column
-	 * @return Array neighbours
-	 */
-	BoardMethods.getNeighbours = function(line, column){
-		return [];
-	};
+    /**
+     * Return the abstract cell
+     *
+     * @param line
+     * @param column
+     * @return bool
+     */
+    BoardMethods.getCell = function(line, column){
+        return this.grid[line][column];
+    };
 
-	/**
-	 * Return the status of the specific cell
-	 *
-	 * @param integer line
-	 * @param integer column
-	 * @return bool
-	 */
-	BoardMethods.getCellStatus = function(line, column){
-		
-	};
+    /**
+     * Return an Array of neighbours
+     * 
+     * @param integer line
+     * @param integer column
+     * @return Array neighbours
+     */
+    BoardMethods.getNeighbours = function(line, column){
 
-	/**
-	 * Will change the status of the cell
-	 * 
-	 * @param integer line
-	 * @param integer column
-	 * @return $('#element')
-	 */
-	BoardMethods.changeCellStatus = function(line, column){
-		
-	};
+        var neighbours = [];
 
+        line = parseInt(line);
+        column = parseInt(column);
+
+        if(
+            typeof this.grid[line-1] !== "undefined"
+            && typeof this.grid[line-1][column-1] !== "undefined"
+        ){
+            neighbours.push(this.grid[line-1][column-1]);
+        }
+
+        if(
+            typeof this.grid[line-1] !== "undefined"
+            && typeof this.grid[line-1][column] !== "undefined" 
+        ){
+            neighbours.push(this.grid[line-1][column]);
+        }
+        
+        if(
+            typeof this.grid[line-1] !== "undefined"
+            && typeof this.grid[line-1][column+1] !== "undefined" 
+        ){
+            neighbours.push(this.grid[line-1][column+1]);
+        }
+        
+        if(typeof this.grid[line][column-1] !== "undefined" ){
+            neighbours.push(this.grid[line][column-1]);
+        }
+        
+        if(typeof this.grid[line][column+1] !== "undefined" ){
+            neighbours.push(this.grid[line][column+1]);
+        }
+        
+        if(
+            typeof this.grid[line+1] !== "undefined" 
+            && typeof this.grid[line+1][column-1] !== "undefined" 
+        ){
+            neighbours.push(this.grid[line+1][column-1]);
+        }
+        
+        if(
+            typeof this.grid[line+1] !== "undefined" 
+            && typeof this.grid[line+1][column] !== "undefined" 
+        ){
+            neighbours.push(this.grid[line+1][column]);
+        }
+        
+        if(
+            typeof this.grid[line+1] !== "undefined" 
+            && typeof this.grid[line+1][column+1] !== "undefined" 
+        ){
+            neighbours.push(this.grid[line+1][column+1]);
+        }
+
+        return neighbours;
+    };
+
+    /**
+     * Return the status of the specific cell
+     *
+     * @param integer line
+     * @param integer column
+     * @return bool
+     */
+    BoardMethods.getCellStatus = function(line, column){
+        return this.grid[line][column];
+    };
+
+    /**
+     * Will change the status of the cell
+     * 
+     * @param integer line
+     * @param integer column
+     * @return $('#element')
+     */
+    BoardMethods.changeCellStatus = function(line, column){
+        this.grid2[line][column] = !this.grid[line][column];
+        return $('#board');
+    };
 
     /**
      * Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
      *
      * @param integer line
-	 * @param integer column
+     * @param integer column
      * @return bool
      */
     BoardMethods.Rule1 = function(line, column){
+        var that = this;
 
+        if( !that.getCellStatus(line, column) ){
+            return "not live";
+        }
+
+        var neighbours = that.getNeighbours(line, column);
+
+        var live_neighbours = neighbours.filter(function(value){
+            return value == true;
+        });
+
+        if( live_neighbours.length < 2 ){
+            that.grid2[line][column] = false;
+            return true;
+        }
+
+        return false;
     };
 
     /**
      * Any live cell with two or three live neighbours lives on to the next generation.
      *
      * @param integer line
-	 * @param integer column
+     * @param integer column
      * @return bool
      */
     BoardMethods.Rule2 = function(line, column){
+        var that = this;
 
+        if( !that.getCellStatus(line, column) ){
+            return "not live";
+        }
+
+        var neighbours = that.getNeighbours(line, column);
+
+        var live_neighbours = neighbours.filter(function(value){
+            return value == true;
+        });
+
+        if( 
+            live_neighbours.length == 2 
+            || live_neighbours.length == 3 
+        ){
+            that.grid2[line][column] = true;
+            return true;
+        }
+
+        return false;
     };
 
     /**
      * Any live cell with more than three live neighbours dies, as if by overpopulation.
      *
      * @param integer line
-	 * @param integer column
+     * @param integer column
      * @return bool
      */
     BoardMethods.Rule3 = function(line, column){
+        var that = this;
 
+        if( !that.getCellStatus(line, column) ){
+            return "not live";
+        }
+
+        var neighbours = that.getNeighbours(line, column);
+
+        var live_neighbours = neighbours.filter(function(value){
+            return value == true;
+        });
+
+        if( live_neighbours.length < 4 ){
+            return false;
+        }
+
+        that.grid2[line][column] = false;
+
+        return true;
     };
 
     /**
      * Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
      *
      * @param integer line
-	 * @param integer column
+     * @param integer column
      * @return bool
      */
     BoardMethods.Rule4 = function(line, column){
+        var that = this;
+
+        if( that.getCellStatus(line, column) ){
+            return "not dead";
+        }
+
+        var neighbours = that.getNeighbours(line, column);
+
+        var live_neighbours = neighbours.filter(function(value){
+            return value == true;
+        });
+
+        if( live_neighbours.length != 3 ){
+            return false;
+        }
+
+        that.grid2[line][column] = true;
+
+        return true;
+    };
+
+    /**
+     * Start the Game of Life
+     *
+     * @return void
+     */
+    BoardMethods.start = function(){
+        // BoardMethods.generateRandomCells();
+        clearInterval(BoardMethods.heartBeat);
+        BoardMethods.heartBeat = setInterval(function(){
+            if( flag ){
+                BoardMethods.updateGrid();
+            }
+        }, 1000);
+    };
+
+    /**
+     * Update abstract grid based on the rules
+     *
+     * @return void
+     */
+    BoardMethods.updateGrid = function(){
+        var that = this;
+
+        that.grid.forEach(function(element, index){
+
+            element.forEach(function(element2, index2){
+
+                var rule1 = that.Rule1(index, index2);
+
+                that.Rule2(index, index2);
+
+                that.Rule3(index, index2);
+
+                var rule4 = that.Rule4(index, index2);
+
+            });
+
+        });
+
+        that.updateGridOne();
+
+        that.grid.forEach(function(element, index){
+
+            element.forEach(function(element2, index2){
+
+                if( that.getCellStatus(index, index2) ){
+                    that.copyCellStatusToDOM( index, index2 );
+                }else{
+                    that.copyCellStatusToDOM( index, index2 );
+                }
+
+            });
+
+        });
 
     };
 
-	return BoardMethods;
+    /**
+     * Change on DOM the status based on the "action"
+     *
+     * @param String action (live|die)
+     * @param int line
+     * @param int column
+     * @return void
+     */
+    BoardMethods.copyCellStatusToDOM = function( line, column ){
+
+        var that = this;
+
+        var current_board,
+            current_line,
+            current_column;
+
+        if( that.getCellStatus(line, column) ){
+
+            current_board = $('#board');
+            current_line = current_board.find('.line')[line];
+            current_column = $(current_line).find('.column')[column];
+            
+            $(current_column).addClass('live');
+
+        }else{
+
+        	current_board = $('#board');
+            current_line = current_board.find('.line')[line];
+            current_column = $(current_line).find('.column')[column];
+
+            $(current_column).removeClass('live');
+
+        }
+
+    };
+
+    /**
+     *
+     */
+    BoardMethods.generateRandomCells = function(){
+    	var that = this;
+
+        var new_value = false;
+
+        that.grid.forEach(function(element, index){
+
+            element.forEach(function(element2, index2){
+                
+                new_value = Math.random() >= 0.5;
+
+                if( new_value ){
+                    that.grid2[index][index2] = true;
+                }
+
+            });
+
+        });
+
+        that.updateGridOne();
+
+        that.updateDom();
+    };
+
+    /**
+     *
+     */
+	BoardMethods.updateDom = function(){
+		var that = this;
+
+		that.grid.forEach(function(element, index){
+
+            element.forEach(function(element2, index2){
+                
+                that.copyCellStatusToDOM(index, index2);
+
+            });
+
+        });
+	};
+
+    /**
+     * Update grid 1 with the value of grid 2
+     */
+    BoardMethods.updateGridOne = function(){
+        var that = this;
+
+        that.grid2.forEach(function(element, index){
+
+            element.forEach(function(element2, index2){
+
+                that.grid[index][index2] = false;
+
+                if( element2 ){
+                    that.grid[index][index2] = true;
+                }
+
+            });
+
+        });
+
+    };
+
+    return BoardMethods;
 };
 /*!
  * jQuery JavaScript Library v3.1.1
--- tests/jasmine/spec/GameOfLifeSpec.js
+++ tests/jasmine/spec/GameOfLifeSpec.js
@@ -3,9 +3,18 @@ describe("Game Of Life", function() {
 
   beforeEach(function() {
     board = new Board($('#board'), 10, 10);
+    jasmine.clock().install();
+  });
+
+  afterEach(function() {
+    jasmine.clock().uninstall();
   });
 
   // Board Tests -------------------------------------------------------------------------------------------
+
+  it("Return the abstract cell.", function(){
+    expect(board.getCell(0,0)).toEqual(board.grid[0][0]);
+  });
 
   it("After instantiate Board, create abstract Grid.", function(){
     expect(board.grid.length).toEqual(10);
@@ -13,8 +22,6 @@ describe("Game Of Life", function() {
   });
 
   it("After instantiate Board, create abstract Grid in the specific tag element.", function(){
-    console.log($('#board').find('.line').length);
-
     expect($('#board').find('.line').length).toEqual(10);
     expect($($('#board').find('.line')[0]).find('.column').length).toEqual(10);
   });
@@ -24,9 +31,17 @@ describe("Game Of Life", function() {
     var neighbours2 = board.getNeighbours(0,1);
     var neighbours3 = board.getNeighbours(0,0);
 
+    var neighbours4 = board.getNeighbours(9,9);
+    var neighbours5 = board.getNeighbours(0,9);
+    var neighbours6 = board.getNeighbours(9,0);
+
     expect(neighbours.length).toEqual(8);
     expect(neighbours2.length).toEqual(5);
     expect(neighbours3.length).toEqual(3);
+
+    expect(neighbours4.length).toEqual(3);
+    expect(neighbours5.length).toEqual(3);
+    expect(neighbours6.length).toEqual(3);
   });
 
   it("getCellStatus must return the real state of the cell.", function(){
@@ -37,6 +52,7 @@ describe("Game Of Life", function() {
   it("after run changeCellStatus, the status of the cell must not be the same as before.", function(){
     board.grid[0][0] = true;
     board.changeCellStatus(0,0);
+    board.updateGridOne();
     expect(board.getCellStatus(0, 0)).toEqual(false);
   });
 
@@ -50,53 +66,202 @@ describe("Game Of Life", function() {
     board.grid[1][1] = false;
 
     board.Rule1(0,0);
+    board.updateGridOne();
 
     expect(board.getCellStatus(0, 0)).toEqual(false);
   });
 
   it("Any live cell with two or three live neighbours lives on to the next generation.", function(){
-    board.grid[0][0] = true;
-
-    board.grid[0][1] = true;
-    board.grid[1][0] = true;
-    board.grid[1][1] = true;
     
-    board.grid[0][9] = true;
+    // board.grid[0][0] = true;
+    if( !board.getCellStatus(0,0) ){
+      board.changeCellStatus(0,0);
+    }
 
-    board.grid[0][8] = true;
-    board.grid[1][9] = true;
-    board.grid[1][8] = false;
+    // board.grid[0][1] = true;
+    if( !board.getCellStatus(0,1) ){
+      board.changeCellStatus(0,1);
+    }
+    // board.grid[1][0] = true;
+    if( !board.getCellStatus(1,0) ){
+      board.changeCellStatus(1,0);
+    }
+    // board.grid[1][1] = true;
+    if( !board.getCellStatus(1,1) ){
+      board.changeCellStatus(1,1);
+    }
+    
+    // --
+    // board.grid[0][9] = true;
+    if( !board.getCellStatus(0,9) ){
+      board.changeCellStatus(0,9);
+    }
 
+    // board.grid[0][8] = true;
+    if( !board.getCellStatus(0,8) ){
+      board.changeCellStatus(0,8);
+    }
+    // board.grid[1][9] = true;
+    if( !board.getCellStatus(1,9) ){
+      board.changeCellStatus(1,9);
+    }
+    // board.grid[1][8] = false;
+    if( board.getCellStatus(1,8) ){
+      board.changeCellStatus(1,8);
+    }
+
+    // --
+    // board.grid[9][9] = true;
+    if( !board.getCellStatus(9,9) ){
+      board.changeCellStatus(9,9);
+    }
+
+    // board.grid[8][8] = true;
+    if( !board.getCellStatus(8,8) ){
+      board.changeCellStatus(8,8);
+    }
+    // board.grid[8][9] = false;
+    if( board.getCellStatus(8,9) ){
+      board.changeCellStatus(8,9);
+    }
+    // board.grid[9][8] = false;
+    if( board.getCellStatus(9,8) ){
+      board.changeCellStatus(9,8);
+    }
+
+    // --
+    board.updateGridOne();
+
+    board.Rule1(0,0);
     board.Rule2(0,0);
-    board.Rule2(0,9);
+    board.Rule3(0,0);
+    board.Rule4(0,0);
 
+    board.Rule1(0,9);
+    board.Rule2(0,9);
+    board.Rule3(0,9);
+    board.Rule4(0,9);
+
+    board.Rule1(9,9);
+    board.Rule2(9,9);
+    board.Rule3(9,9);
+    board.Rule4(9,9);
+
+    board.updateGridOne();
+    
     expect(board.getCellStatus(0, 0)).toEqual(true);
     expect(board.getCellStatus(0, 9)).toEqual(true);
+    expect(board.getCellStatus(9, 9)).toEqual(false);
   });
 
   it("Any live cell with more than three live neighbours dies, as if by overpopulation.", function(){
-    board.grid[1][1] = true;
+    // board.grid[1][1] = true;
+    if( !board.getCellStatus(1,1) ){
+      board.changeCellStatus(1,1);
+    }
 
-    board.grid[0][0] = true;
-    board.grid[0][1] = true;
-    board.grid[1][0] = true;
-    board.grid[0][2] = true;
+    // board.grid[0][0] = true;
+    if( !board.getCellStatus(0,0) ){
+      board.changeCellStatus(0,0);
+    }
+    // board.grid[0][1] = true;
+    if( !board.getCellStatus(0,1) ){
+      board.changeCellStatus(0,1);
+    }
+    // board.grid[1][0] = true;
+    if( !board.getCellStatus(1,0) ){
+      board.changeCellStatus(1,0);
+    }
+    // board.grid[0][2] = true;
+    if( !board.getCellStatus(0,2) ){
+      board.changeCellStatus(0,2);
+    }
+
+    board.updateGridOne();
 
     board.Rule3(1,1);
+
+    board.updateGridOne();
 
     expect(board.getCellStatus(1, 1)).toEqual(false);
   });
 
   it("Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.", function(){
-    board.grid[0][0] = false;
+    // board.grid[0][0] = false;
+    if( board.getCellStatus(0,0) ){
+      board.changeCellStatus(0,0);
+    }
 
-    board.grid[0][1] = true;
-    board.grid[1][0] = true;
-    board.grid[1][1] = true;
+    // board.grid[0][1] = true;
+    if( !board.getCellStatus(0,1) ){
+      board.changeCellStatus(0,1);
+    }
+    // board.grid[1][0] = true;
+    if( !board.getCellStatus(1,0) ){
+      board.changeCellStatus(1,0);
+    }
+    // board.grid[1][1] = true;
+    if( !board.getCellStatus(1,1) ){
+      board.changeCellStatus(1,1);
+    }
+
+    board.updateGridOne();
 
     board.Rule4(0,0);
 
+    board.updateGridOne();
+    
     expect(board.getCellStatus(0, 0)).toEqual(true);
+  });
+
+  // TODO: not working well
+  it("DOM must be exactly the same as the abstract.", function(){
+
+    board.generateRandomCells();
+    board.updateGrid();
+
+    window.the_same = true;
+
+    Object
+      .keys($('.line'))
+      .map(function (key) {
+        return $('.line')[key]; 
+      })
+      .filter(function(element){
+        return $(element).hasClass('line');
+      })
+      .forEach(function(element, index){ 
+
+        Object
+          .keys($(element).find('.column'))
+          .map(function(key2){
+            return $(element).find('.column')[key2];
+          })
+          .filter(function(element){
+            return $(element).hasClass('column');
+          })
+          .forEach(function(element2, index2){
+
+            if( 
+              (
+                board.getCellStatus(index, index2) == false
+                && $(element2).hasClass('live') == true
+              )
+              || (
+                board.getCellStatus(index, index2) == true
+                && $(element2).hasClass('live') == false
+              )
+            ){
+              window.the_same = false;
+            }
+
+          });
+
+      });
+
+    jasmine.clock().tick(1001);
+
+    expect(window.the_same).toEqual(true);
   });
 
 });
